02646be897ad94d04f773a4e4a671464a8758992
lateralusd <xdaemonx@protonmail.ch>
2021-06-19 19:26:15 +0200

    Use new name

--- GetASLR.py
+++ GetASLR.py
@@ -1,24 +1,32 @@
 import lldb
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+END='\033[0m'
+
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f GetASLR.getOffset getASLR')
+    debugger.HandleCommand('command script add -f GetASLR.getOffset aslr')
 
 def getOffset(debugger, command, result, internal_dict):
     target = debugger.GetSelectedTarget()
 
-    module_name = command
     module_index = 0
-    found = False
 
-    for module in target.module_iter():
-        if module_name == module.GetFileSpec().GetFilename():
-            found = True
-            break
-        module_index += 1
+    if command != "":
+        module_name = command
 
-    if not found:
-        print('[-] Could not find module')
-        return
+        found = False
+
+        for module in target.module_iter():
+            if module_name == module.GetFileSpec().GetFilename():
+                found = True
+                break
+            module_index += 1
+
+        if not found:
+            print('\033[0;31m[-] Could not find module\033[0m')
+            return
 
     base_address = int(target.GetModuleAtIndex(module_index).GetObjectFileHeaderAddress().GetFileAddress())
     load_address = int(target.GetModuleAtIndex(module_index).GetObjectFileHeaderAddress().GetLoadAddress(target))
